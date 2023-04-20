2e4f812696b0d0cb0cd4212578c913e3dfeb97cb
loretari <loretari@gmail.com>
2023-04-20 10:27:52 +0300

    init: add Checkout in Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import { useContext} from 'react';
-import React from 'react';
+import { useContext, useState} from 'react';
+
 import classes from './Cart.module.css';
 import Modal from '../UI/Modal';
 import CartContext from '../../store/cart-context'
 import CartItem from './CartItem';
+import Checkout from './Checkout';
 
 const Cart = (props) => {
+
+    const [isCheckout, setIsCheckout] = useState(false);
      const cartCtx =useContext(CartContext);
 
      const totalAmount = `$${Math.max(cartCtx.totalAmount, 0).toFixed(2)}`;
@@ -18,6 +21,10 @@ const Cart = (props) => {
      const cartItemAddHandler = (item )=> {
          cartCtx.addItem({...item, amount: 1});
      };
+
+     const orderHandler = () => {
+setIsCheckout(true);
+     }
 
     const cartItems = (
         <ul className={classes['cart-Items']}>
@@ -34,6 +41,18 @@ const Cart = (props) => {
         </ul>
     );
 
+     const modalActions = (
+         <div className={classes.actions}>
+             <button className={classes['button--alt']} onClick={props.onClose}>
+                 Close
+             </button>
+             {hasItems && < button className={classes.button} onClick={orderHandler}>
+                 Order
+             </button>}
+
+         </div>
+     )
+
     return (
         <Modal onClose = {props.onClose}>
             {cartItems}
@@ -41,15 +60,8 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
         </div>
-    <div className={classes.actions}>
-        <button className={classes['button--alt']} onClick={props.onClose}>
-            Close
-        </button>
-       {hasItems && < button className={classes.button}>
-           Order
-       </button>}
-
-    </div>
+           {isCheckout && <Checkout />}
+            {!isCheckout && modalActions}
         </Modal>
     )
 }
