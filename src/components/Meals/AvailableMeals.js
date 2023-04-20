443f7837eec8039f346c0e806c202c1d87b78cb7
loretari <loretari@gmail.com>
2023-04-20 09:54:55 +0300

    init: add meals.json

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -14,7 +14,7 @@ const AvailableMeals = () => {
 
     useEffect(() => {
         const fetchMeals = async  () => {
-          const response = await fetch('https://react-http-cd156-default-rtdb.firebaseio.com/meals.');
+          const response = await fetch('https://react-http-cd156-default-rtdb.firebaseio.com/meals.json');
 
           if (!response.ok) {
               throw new Error('Something went wrong!')
